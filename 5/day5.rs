bd130075840890fba9f33ca5a228118dc00dbbbf
Pengler38 <70914054+Pengler38@users.noreply.github.com>
2024-08-06 00:00:39 -0400

    Day 5: Parsing of input complete

--- 5/day5.rs
+++ 5/day5.rs
@@ -1,6 +1,70 @@
 //ppengler
 //day5.rs 
 
+use std::fs;
+use std::env;
+
+struct Map{
+    dest: u32,
+    src: u32,
+    range: u32,
+}
 
 fn main() {
+
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print!("Usage: ./day5rs.exe ");
+        return;
+    }
+    //let input = fs::read_to_string(env::args().collect::<Vec<String>>()[0]).expect("File not found");
+    let input = fs::read_to_string(args[1].as_mut_str()).expect("File not found");
+    let mut lines = input.lines();
+    //let mut lines = input.lines();
+    
+    //eprint!("{}\n", lines.peek().unwrap());
+
+
+    //Parse in the seed numbers
+    let mut lineparse: Vec<&str> = lines.next().unwrap().split(' ').collect();
+    lineparse.remove(0); //remove the "seeds:"
+    println!("{}", lineparse[0]);
+    let mut seeds: Vec<u32> = vec![];
+    for word in lineparse {
+        seeds.push(word.parse::<u32>().unwrap());
+    }
+
+    //Parse in the maps
+    let mut maps: Vec<Vec<Map>> = Vec::new();
+    for _x in 0..7{
+       let mut map: Vec<Map> = Vec::new();
+       parse(":", &mut map, &mut lines);
+       maps.push(map);
+    }
+}
+
+//calls lines.next() until start is found, then on the next line, read in the map values until a
+//\n\n is found or the end of lines
+fn parse(start: &str, arr: &mut Vec<Map>, lines: &mut std::str::Lines){
+    while !lines.next().unwrap().contains(start) { }
+    loop {
+        let next : &str; 
+        //Ensure we have a next line
+        match lines.next() {
+            Some(x) => next = x,
+            None => break,
+        };
+        //Ensure the line is not empty
+        if !next.contains(char::is_numeric) {return;}
+
+        println!("{next}");
+        let numbers: Vec<&str> = next.split(' ').collect();
+        arr.push(
+            Map{
+            dest: numbers[0].parse().unwrap(),
+            src: numbers[1].parse().unwrap(),
+            range: numbers[2].parse().unwrap(),
+            }
+            );
+    }
 }
--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 flags = -Wall -Wextra -O1
 
-executables = 1/day1.exe 1/day1_2.exe 2/day2.exe 2/day2_2.exe 3/day3.exe 3/day3_2.exe 4/target/debug/day4.exe
+executables = 1/day1.exe 1/day1_2.exe 2/day2.exe 2/day2_2.exe 3/day3.exe 3/day3_2.exe 4/target/debug/day4.exe 5/day5rs.exe
 
 all: $(executables)
 
@@ -20,6 +20,9 @@ all: $(executables)
 
 4/target/debug/day4.exe: 4/Cargo.toml 4/day4.rs
 	cargo build --manifest-path 4/Cargo.toml
+
+%rs.exe: %.rs
+	rustc $^ -o $@
 
 %.exe: %.cpp
 	g++ $^ $(flags) -o $@
